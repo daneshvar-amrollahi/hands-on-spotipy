7a11cd8d2907c956770b3baa3ecc1e182f6b73e7
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2022-06-25 23:10:40 +0200

    feat: open a playlist in browser

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import webbrowser
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    print("""
+███████ ███████ ██      ███████ 
+██      ██      ██      ██      
+█████   █████   ██      ███████ 
+██      ██      ██           ██ 
+███████ ███████ ███████ ███████ 
+                                                   
+""")
+
+    print("******************************************************")
+    print("Welcome to Eels-Bot!")
+    print("******************************************************")
+
+    print("What do you want to listen to from this lovely band?")
+    print("1 - Spotify's Eels Essentials")
+    print("2 - Daneshvar's Picks")
+    print("Just enter your number:", end = " ")
+
+    q = int(input())
+
+    if q == 1:
+        webbrowser.open("https://open.spotify.com/playlist/37i9dQZF1DZ06evO24NfX2?si=0b690e33647f444e&nd=1")
+    if q == 2:
+        webbrowser.open("https://open.spotify.com/playlist/4LuLZqAtpf4To5rPCz8p5a?si=530714c88dd34eca&nd=1")
+
+    print("Cool new features coming soon!")
